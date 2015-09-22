bf50f7af41868710a3ce506c7b0f1642e1bbccb6
ctravis <ctravis1@student.cvtc.edu>
2015-09-22 01:19:52 +0000

    delete spring

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919004824) do
+ActiveRecord::Schema.define(version: 20150922003724) do
 
   create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.text     "markdown"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "tag"
   end
+
+  add_index "notes", ["tag"], name: "index_notes_on_tag"
 
 end
