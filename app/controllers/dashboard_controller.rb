f8a48da869b9698584918bf237e0ded991060e0f
Devon Jones <djones9898@hotmail.com>
2015-10-06 04:50:51 +0000

    styled the dashboard add tag cloud to tags list page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,7 @@
 class DashboardController < ApplicationController
   def show
     @notes = Note.where(active: true).order(updated_at: :desc).limit(10)
-    @tags = ActsAsTaggableOn::Tag.most_used(100)
+    @tags = ActsAsTaggableOn::Tag.most_used(20)
     @topscore = ActsAsTaggableOn::Tag.maximum(:taggings_count)
   end
 
