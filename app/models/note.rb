695edf8146cf737d2348e2b23a716abff5dcf19c
Devon Jones <djones9898@hotmail.com>
2015-10-09 14:04:02 +0000

    add title to note's with text scope

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,7 +2,7 @@ class Note < ActiveRecord::Base
   validates :title, :presence => true, :length => { :maximum => 250 }
   validates :markdown, :presence => true
   acts_as_taggable
-  scope :with_text, -> (text) {where("markdown like ?", "%#{text}%")}
+  scope :with_text, ->(text) {where("markdown like :text or title like :text", text: "%#{text}%")}
   scope :active, -> {where("active=?",true)}
   scope :inactive, -> {where(active: false)}
 
