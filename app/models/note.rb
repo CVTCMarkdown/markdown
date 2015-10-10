32c871dfb4562b6a964c9b26fbc65f8e30198671
Devon Jones <djones9898@hotmail.com>
2015-10-10 00:16:16 -0500

    refactor note model & controller

--- app/models/note.rb
+++ app/models/note.rb
@@ -3,7 +3,7 @@ class Note < ActiveRecord::Base
   validates :markdown, :presence => true
   acts_as_taggable
   scope :with_text, ->(text) {where("markdown like :text or title like :text", text: "%#{text}%")}
-  scope :active, -> {where("active=?",true)}
+  scope :active, -> {where(active: true)}
   scope :inactive, -> {where(active: false)}
 
   def share
