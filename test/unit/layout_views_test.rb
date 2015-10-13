ab6d5eea91819cf30bd9266f8cdd19fcdc42628e
ctravis <ctravis1@student.cvtc.edu>
2015-10-13 00:38:47 +0000

    Fixed Tests

--- test/unit/layout_views_test.rb
+++ test/unit/layout_views_test.rb
@@ -5,33 +5,20 @@ class LayoutViewsTest < ActionView::TestCase
   
   setup do
     
-    @user = User.create(email: 'user@markdown.com', password: 'password')
+    @user = User.create(email: 'user@markdown.com', password: '12345678')
     @user2 = User.create(email: 'user2@markdown.com', password: 'password')
 
     sign_in @user
-
-    @note1 = Note.create(title: 'title1', markdown:'markdown1', active:true)
-    @user.note << @note1
-    @user.tag(@note1, :with => 'tag1, tag2, tag3', :on => 'tags')
-
-    @note2 = Note.create(title: 'title2', markdown: 'markdown2', active:false)
-    @user.note << @note2
-    @user.tag(@note2, :with => 'tag3, tag4, tag5', :on => 'tags')
-
-    @note3 = Note.create(title: 'title3', markdown: 'markdown3', active: true)
-    @user2.note << @note3
-    @user2.tag(@note3, :with => 'tag1, tag2, tag3, tag4', :on => 'tags')
-    
   end
   
   test "application layout has global links" do
-    render file: 'layouts/application'
+    render file: 'layouts/application'#, locals: {current_user: @user}
     
-    assert_select "a", {text: @user.email}
-    assert_select 'a[herf=?]', dashboard_path, {text: 'Dashboard'}
-    assert_select 'a[herf=?]', new_note_path, {text: 'New Note'}
-    assert_select 'a[herf=?]', trashed_notes_path, {text: 'Trash Can'}
-    assert_select 'a[herf=?]', destroy_user_session_path, {text: 'Sign Out'}
+    assert_select 'a[href=?]', edit_user_registration_path, {text: @user.email}
+    assert_select 'a[href=?]', dashboard_path, {text: 'Dashboard'}
+    assert_select 'a[href=?]', new_note_path, {text: 'New Note'}
+    assert_select 'a[href=?]', trashed_notes_path, {text: 'Trash Can'}
+    assert_select 'a[href=?]', destroy_user_session_path, {text: 'Sign Out'}
   end
   
 end
